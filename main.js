a1c9420f49338d24c03cdd661e6099e6a7e8657d
Sobarzo14 <63866666+Sobarzo14@users.noreply.github.com>
2024-11-23 16:52:19 -0500

    Added comments to the files

--- main.js
+++ main.js
@@ -12,28 +12,18 @@ const peopleUrl = new URL("./public/models/populate_idle_models_2000_frames.glb"
 const buildings = new URL('./public/images/buildings.jpg', import.meta.url);
 const stars = new URL('./public/images/stars.jpg', import.meta.url);
 
-
-
+// Renderer
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(
-  75,
-  window.innerWidth / window.innerHeight,
-  0.1,
-  1000
-);
-
+// Texture and GTFL Loaders
 const loader = new GLTFLoader();
 const textureLoader = new THREE.TextureLoader();
 const ctLoader = new THREE.CubeTextureLoader();
-const orbit = new OrbitControls(camera, renderer.domElement);
 
-const axesHelper = new THREE.AxesHelper(5);
-scene.add(axesHelper);
-
+// Scene
+const scene = new THREE.Scene();
 scene.background = ctLoader.load([
   buildings.href,
   buildings.href,
@@ -43,20 +33,21 @@ scene.background = ctLoader.load([
   buildings.href
 ])
 
-
+//Camera
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
 camera.position.set(0, 100, 200);
+
+
+// Orbit controls
+const orbit = new OrbitControls(camera, renderer.domElement);
 orbit.update();
 
-const dl = new THREE.DirectionalLight(0xFFFFFF, 0.8);
-dl.position.y = 200;
-const dlHelper = new THREE.DirectionalLightHelper(dl, 3)
-scene.add(dlHelper)
-scene.add(dl);
-const al = new THREE.AmbientLight(0xFFFFFF, 0.8);
-al.position.set(0, 100, 200);
-
-
-scene.add(al);
+// Added models
 loader.load(stageUrl.href, function (gltf) {
   const model = gltf.scene;
   scene.add(model)
@@ -69,6 +60,15 @@ loader.load(peopleUrl.href, function (gltf) {
   scene.add(model)
 })
 
+// Added lighting
+const dl = new THREE.DirectionalLight(0xFFFFFF, 0.8);
+dl.position.y = 200;
+const dlHelper = new THREE.DirectionalLightHelper(dl, 3)
+scene.add(dlHelper)
+scene.add(dl);
+const al = new THREE.AmbientLight(0xFFFFFF, 0.8);
+al.position.set(0, 100, 200);
+scene.add(al);
 
 function animate(time) {
 
